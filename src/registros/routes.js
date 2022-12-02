6c877ac1ff8c0dd8e5119866c47ea8aa3969ac8c
Sabrina <sabrina.rf2003@gmail.com>
2022-12-02 13:32:20 -0300

    Cadastro e autenticação criados

--- src/registros/routes.js
+++ src/registros/routes.js
@@ -7,8 +7,8 @@ const controller = new RegistrosController();
 
 router.post('/', isAuth, (req, res) => controller.create(req, res));
 router.get('/list', (req, res) => controller.list(req, res));
-router.get('/:id', (req, res) => controller.detail(req, res));
-router.put('/:id', isAuth, (req, res) => controller.update(req, res));
-router.delete('/:id', isAuth, (req, res) => controller.delete(req, res));
+//router.get('/:id', (req, res) => controller.detail(req, res));
+//router.put('/:id', isAuth, (req, res) => controller.update(req, res));
+//router.delete('/:id', isAuth, (req, res) => controller.delete(req, res));
 
 module.exports = router;
